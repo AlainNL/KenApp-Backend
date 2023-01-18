b9d87835b6b9392342e3d47fb5c11c8a8c013064
Alain Noel <alain.noel34@gmail.com>
2023-01-18 02:22:59 +0100

    connect route front/back ok

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const defibrillateurRoutes = require("./src/defibrillateur/routes");
+
 
 
 const app = express()
 const port = 3000
 
+app
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send("start"))
 
